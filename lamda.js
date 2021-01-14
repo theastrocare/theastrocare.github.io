7feeac212bae889e32f838b7e50f905b435abb4a
Divesh Keswani <diveshkswn@gmail.com>
2021-01-14 19:43:09 +0530

    1. Loading animation while form submittion is done. 2. Some alignment changed are also done .

--- lamda.js
+++ lamda.js
@@ -4,6 +4,8 @@ URL = 'https://jig1qbpypa.execute-api.us-east-1.amazonaws.com/PROD/';
 $(document).ready(function () {
 
     $("#mainForm").submit(function (e) {
+        //at this line loading animation css will be set to visible so that is should be visible on front end.
+        $("#loading-id").css('visibility', 'visible');
         e.preventDefault();
         
         var name = $("#Name").val(),
@@ -32,6 +34,8 @@ $(document).ready(function () {
             if (error != '') $('#form-response').text('Error: ' + error);
         }).always(function(data) {
             console.log(JSON.stringify(data));
+            //At this line loading animation will hide again.
+            $("#loading-id").css('visibility', 'hidden');
             $('#form-response').text('Your Details have been submitted. Thank You!');
         });
 
